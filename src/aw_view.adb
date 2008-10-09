a834a0660be6e34b4ef97d5a72fb42457611308f
Eduardo Calderini Jr <eduardo.calderini@gmail.com>
2008-10-09 18:01:52 +0000

    main structure for Aw_View created. Time to code
    git-svn-id: http://dev.adaworks.net/svn/trunk/awview@509 115cb82a-aa27-0410-86a0-a2651309cc67

---
+++ src/aw_view.adb
@@ -0,0 +1,8 @@
+package body Aw_View is
+
+	procedure dummie is
+		-- for testing only
+	begin
+		null;
+	end dummie;
+end Aw_View;
---
+++ src/aw_view.ads
@@ -0,0 +1,12 @@
+-- Main package for Aw_View
+--
+-- author: Eduardo Calderini Jr <ecalderini@ydeasolutions.com.br>
+
+
+
+
+package Aw_View is
+
+	procedure dummie;
+	-- for testing only
+end Aw_View;
