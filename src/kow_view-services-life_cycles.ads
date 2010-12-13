d0f6f9ee6765a3ed95a30b5b6410653add491330
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2010-12-13 14:47:25 -0200

    working on the design

---
+++ src/kow_view-services-life_cycles.ads
@@ -0,0 +1,65 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+
+
+-- Interface for handling service life cycles
+
+
+
+---------
+-- AWS --
+---------
+with AWS.Status;
+with AWS.Response;
+
+
+-------------------
+-- KOW Framework --
+-------------------
+with KOW_View.Components;
+
+
+package KOW_View.Services.Life_Cycles is
+
+
+	type Life_Cycle_Manager is interface;
+
+
+	procedure Process_Request(
+				Service : in out KOW_View.Components.Service_Type'Class;
+				Request	: in     AWS.Status.Data;
+				Response:    out AWS.Response.Data
+			) is abstract;
+	-- process the request...
+
+
+end KOW_View.Services.Life_Cycles;
