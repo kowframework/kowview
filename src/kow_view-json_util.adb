928bec1cdfe3c06bdcb22ec942a0977d69106f2e
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2010-12-13 20:43:38 -0200

    adding json_util

---
+++ src/kow_view-json_util.adb
@@ -0,0 +1,76 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+
+--------------
+-- Ada 2005 --
+--------------
+with Ada.Exceptions;
+
+
+
+-------------------
+-- KOW Framework --
+-------------------
+with KOW_Lib.Json;			use KOW_Lib.Json;
+with KOW_Lib.String_Util;
+
+---------
+-- AWS --
+---------
+with AWS.Messages;
+with AWS.Response;
+
+package body KOW_View.json_util is
+
+
+	function Build_Error(
+			E		: Ada.Exceptions.Exception_Occurrence;
+			Status_Code	: AWS.Messages.Status_Code := AWS.Messages.S505
+		) return AWS.Response.Data is
+		
+		use Ada.Exceptions;
+		use KOW_Lib.String_Util;
+
+
+		Object	: Object_Type;
+	begin
+		Set( Object, "status", "error" );
+		Set( Object, "error", Exception_Name( e ) );
+		Set( Object, "message", Exception_Message( e ) );
+		return AWS.Response.Build(
+					Content_Type	=> "application/json", 
+					Message_Body	=> To_Json( Object ),
+					Status_Code	=> Status_Code
+			);
+	end Build_Error;
+
+end KOW_View.json_util;
--- src/kow_view-json_util.ads
+++ src/kow_view-json_util.ads
@@ -29,56 +29,26 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-
-
-
+------------------------------------------------------------------------------
+-- Some tools for Helping treating JSon Data                                --
+------------------------------------------------------------------------------
 
 
 --------------
 -- Ada 2005 --
 --------------
-with Ada.Containers.Ordered_Maps;
-with Ada.Strings.Unbounded;		use Ada.Strings.Unbounded;
-
-
--------------------
--- KOW Framework --
--------------------
-with KOW_Config;
+with Ada.Exceptions;
 
 
 ---------
 -- AWS --
 ---------
-with AWS;
+with AWS.Messages;
 with AWS.Response;
-with AWS.Status;
 
-
-package KOW_View.Service_Mapping is
-
-
-
-	type Service_Map_Type is record
-		Component_Name	: Unbounded_String;
-		Service_Name	: Unbounded_String;
-		Config		: KOW_Config.Config_File;
-	end record;
-
-
-	package Service_Maps is new Ada.Containers.Ordered_Maps(
-				Key_Type	=> Unbounded_String,
-				Element_Type	=> Service_Map_Type
-			);
-
-	procedure Reload_Mappings;
-
-
-	function AWS_Callback( Request : in AWS.Status.Data ) return AWS.Response.Data;
-
-
-private
-	Mapping		: Service_Maps.Map;
-	Default_Service	: Unbounded_String; -- "/page"
-
-end KOW_View.Service_Mapping;
+package KOW_View.json_util is
+	function Build_Error(
+			E		: Ada.Exceptions.Exception_Occurrence;
+			Status_Code	: AWS.Messages.Status_Code := AWS.Messages.S505
+		) return AWS.Response.Data;
+end KOW_View.json_util;
