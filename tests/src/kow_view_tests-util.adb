fed23aa7ac005e66beb2fd9c134edc80bcf0c4d9
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2010-12-14 11:11:56 -0200

    adding testing framework

---
+++ tests/src/kow_view_tests-util.adb
@@ -0,0 +1,74 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- Tests for the utility package                                            --
+------------------------------------------------------------------------------
+
+
+-----------
+-- Ahven --
+-----------
+with Ahven;
+with Ahven.Framework;
+
+
+
+-------------------
+-- KOW Framework --
+-------------------
+with KOW_View.Util;		use KOW_View.Util;
+
+package body KOW_View_Tests.Util is
+
+
+	overriding
+	procedure Initialize( T : in out Test_Type ) is
+	begin
+		Set_Name( T, "KOW_View.Util" );
+		Ahven.Framework.Add_Test_Routine( T, Test_Get_Type_Name'Access, "Get_Type_Name" );
+	end Initialize;
+
+
+	procedure Test_Get_Type_Name is
+		type Meu_Tipo_Type is tagged null record;
+
+		Expected_Name : constant String := "meu_tipo";
+		Computed_Name : constant String := Get_Type_Name( Meu_Tipo_Type'Tag );
+	begin
+		Ahven.Assert(
+				Condition	=> Expected_Name = Computed_Name,
+				Message		=> Computed_Name & " is not valid (expected " & Expected_name & ")";
+			);
+	end Test_Get_Type_Name;
+
+
+end KOW_View_Tests.Util
---
+++ tests/src/kow_view_tests.adb
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- Main package for testing in KOW View module                              --
+------------------------------------------------------------------------------
+
+
+
+-----------
+-- Ahven --
+-----------
+with Ahven.Framework;
+
+-----------
+-- Tests --
+-----------
+with KOW_View_Tests.Util;
+
+package body KOW_View_Tests is
+
+
+begin
+	Suite := Ahven.Framework.Create_Suite( "KOW View Tests" );
+
+	Ahven.Framework.Add_Test( Suite.all, new KOW_View_Tests.Util.Test_Type );
+
+end KOW_View_Tests;
