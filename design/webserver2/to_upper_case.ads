076c3921d4c6aaf73828b1630cb000b121057f97
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2012-08-21 20:15:32 -0300

    removing old design files

--- design/webserver2/to_upper_case.ads
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO;	use Ada.Text_IO;
-
-package to_upper_case is
-
-	Factor: constant Integer   := Character'Pos( 'a' ) - Character'Pos( 'A' );
-	function To_Up( C: in Character ) return Character;
-	procedure convert	( C: in Character );
-
-end to_upper_case;
