549f317b9be5a0c2ec8ef861d987b82b11deb367
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2012-08-27 18:56:40 -0300

    moving on with the implementation. now there are some static file modules

---
+++ src/component/kow_view-modules.adb
@@ -0,0 +1,216 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+pragma License (GPL);
+
+
+
+--------------
+-- Ada 2005 --
+--------------
+with Ada.Streams.Stream_IO;
+with Ada.Strings.Unbounded;
+
+
+
+
+
+-------------------
+-- KOW Framework --
+-------------------
+with KOW_Lib.File_System;
+with KOW_View.Components;	use KOW_View.Components;
+with KOW_View.Pages;		use KOW_View.Pages;
+
+
+
+package body KOW_View.Modules is
+
+
+	-----------------
+	-- Base Module --
+	-----------------
+
+	function Get_Name( Module : in Base_Module ) return String is
+	begin
+		return KOW_View.Util.Get_Type_Name( Base_Module'Class( Module )'Tag, "_module" );
+	end Get_Name;
+
+	function Locate_Resource(
+			Module		: in Base_Module;
+			Status		: in Request_Status_Type;
+			Resource	: in String;
+			Extension	: in String := "";
+			Kind		: in Ada.Directories.File_Kind := Ada.Directories.Ordinary_File;
+		) return String is
+		use KOW_Lib.File_System;
+	begin
+		return Locate_Resource(
+					Component	=> Module.Component.all,
+					Status		=> Status,
+					Resource	=> Get_Name( Base_Module'Class( Module ) ) & "_module" / Resource,
+					Extension	=> Extension,
+					Kind		=> Kind
+				);
+	end Locate_Resource;
+
+	-----------------
+	-- KTML Module --
+	-----------------
+
+	overriding
+	procedure Process_Request(
+				Module	: in out KTML_Module;
+				Page	: in out Page_Interface'Class;
+				Status	: in     Request_Status_Type
+			) is
+		Initial_State : KOW_Lib.Json.Object_Type;
+
+
+		function Template return String is
+			Tpl : constant String := "template";
+			use KOW_Lib.Json;
+		begin
+			if Contains( Initial_State, Tpl ) then
+				return Get( Initial_State, Tpl );
+			else
+				return "success";
+			end if;
+		end Template;
+	begin
+		Process_Json_Request(
+					Module	=> KTML_Module'Class( Module ),
+					Page	=> Page,
+					Status	=> Status,
+					Response=> Initial_State
+				);
+		Append(
+				Page	=> Page,
+				Content	=> KOW_View.KTML.Render(
+								File_Path	=> Locate_Resource(
+												Module		=> KTML_Module'Class( Module ),
+												Status		=> Status,
+												Resource	=> Template,
+												Extension	=> "ktml",
+												Kind		=> Ada.Directories.Ordinary_File
+											),
+								Initial_State	=> Initial_State
+							)
+					);
+	end Process_Request;
+
+
+
+	-------------------
+	-- Static Module --
+	-------------------
+
+
+
+	generic
+	package Static_Modules is
+
+		function Get_Contents ( Path : in String ) return String is
+			-- TODO :: figure out how to create a cache of some sort
+			use Ada.Strings.Unbounded;
+			use Ada.Streams.Stream_IO;
+
+			F : File_Type;
+			S : Stream_Access;
+			Buffer : Unbounded_String;
+		begin
+
+			Open(
+					File	=> F,
+					Mode	=> In_File,
+					Name	=> Path
+				);
+			S := Stream( F );
+			while not End_Of_File( F ) loop
+				Append( Buffer, Character'Input( S ) );
+			end loop;
+
+			Close( F );
+			return To_String( Buffer );
+		end Get_Contents;
+
+		overriding
+		procedure Process_Request(
+					Module	: in out Static_Module;
+					Page	: in out Page_Interface'Class;
+					Status	: in     Request_Status_Type
+				) is
+
+			Path : constant String := Locate_Resource(
+									Module		=> Static_Module'Class( Module ),
+									Status		=> Status,
+									Resource	=> Resource,
+									Extension	=> "html",
+									Kind		=> Ada.Directories.Ordinary_File
+								);
+		begin
+			Append(
+					Page		=> Page,
+					Contents	=> Get_Contents( Path )
+				);
+		end Process_Request;
+		
+		overriding
+		procedure Process_Json_Request(
+					Module	: in out Static_Module;
+					Page	: in out Page_interface'Class;
+					Status	: in     Request_Status_Type;
+					Response:    out KOW_Lib.Json.Object_Type
+				) is
+		begin
+			raise program_error with "can't serve static files in JSON requests";
+		end Process_Json_Request;
+
+		--------------------
+		-- Module Factory --
+		--------------------
+
+		overriding
+		procedure Create(
+					Factory	: in out Static_Module_Factory;
+					Module	:    out Module_Ptr
+			) is
+		begin
+			Module := The_Module;
+		end Create;
+	
+		overriding
+		procedure Destroy(
+					Factory	: in out Static_Module_Factory;
+					Module	: in out Module_Ptr
+				) is
+		begin
+			Module := null;
+		end Destroy;
+
+	end Static_Modules;
+
+
+
+end KOW_View.Modules;
+
---
+++ src/component/kow_view-modules.ads
@@ -0,0 +1,161 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          KOW Framework :: View                           --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWView is free software; you can redistribute it  and/or modify it under--
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWView is distributed in the hope that it will be useful,but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWView; see file COPYING.  If not, write--
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+pragma License (GPL);
+
+--------------
+-- Ada 2005 --
+--------------
+with Ada.Directories;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO;
+
+
+
+
+-------------------
+-- KOW Framework --
+-------------------
+with KOW_View.Components;	use KOW_View.Components;
+with KOW_View.Pages;		use KOW_View.Pages;
+
+
+
+package KOW_View.Modules is
+
+
+	-----------------
+	-- Base Module --
+	-----------------
+
+	type Base_Module( Component : Component_Ptr ) is abstract new Module_Interface with null record;
+	-- a module to be used as base to other module implementations
+
+
+	function Get_Name( Module : in Base_Module ) return String;
+	-- giving the module type is name SOMETHING_Module, returns SOMETHING
+
+	function Locate_Resource(
+			Module		: in Base_Module;
+			Status		: in Request_Status_Type;
+			Resource	: in String;
+			Extension	: in String := "";
+			Kind		: in Ada.Directories.File_Kind := Ada.Directories.Ordinary_File;
+		) return String;
+	-- locate resource using the Component's locate_resource method
+	-- the resource will be:
+	-- [module_name]_module/resource
+
+
+
+	-----------------
+	-- KTML Module --
+	-----------------
+
+	type KTML_Module ( Component : Component_Ptr ) is abstract new Base_Module (Component) with null record;
+	-- render a template using the results from Process_Json_Request
+	-- the json procedure should determine if it's being called by the page ASYNC
+	-- Json request handler or is rendering the HTML by the Status.Mode variable
+	--
+	-- the template name should be set by the process_json_request, using the template variable within the returning
+	-- object
+	-- will use the "success.ktml" by default.
+
+
+	overriding
+	procedure Process_Request(
+				Module	: in out KTML_Module;
+				Page	: in out Page_Interface'Class;
+				Status	: in     Request_Status_Type
+			);
+
+	-------------------
+	-- Static Module --
+	-------------------
+
+
+
+	generic
+		Component : Component_Ptr;
+		Resource  : constant String;
+		-- the resource to be served (to be located by module's Locate_Resource function)
+		-- the "html" extension is always used
+	package Static_Modules is
+
+		-- instances of this package appends the given file as text
+		-- in the page service
+
+		-- TODO :: implement cache
+
+		----------------
+		-- The Module --
+		----------------
+
+		type Static_Module is new Base_Module ( Component ) with null record;
+
+		overriding
+		procedure Process_Request(
+					Module	: in out Static_Module;
+					Page	: in out Page_Interface'Class;
+					Status	: in     Request_Status_Type
+				);
+		-- append the File_Path contents into the page
+		
+		overriding
+		procedure Process_Json_Request(
+					Module	: in out Static_Module;
+					Page	: in out Page_interface'Class;
+					Status	: in     Request_Status_Type;
+					Response:    out KOW_Lib.Json.Object_Type
+				);
+		-- raises program_error with "can't serve static files in JSON requests"
+
+
+		-----------------
+		-- The Factory --
+		-----------------
+
+		type Static_Module_Factory is new Module_Factory_Interface with null record;
+		-- return a singleton object
+		-- destroy does only sets the pointer to null
+
+		overriding
+		procedure Create(
+					Factory	: in out Static_Module_Factory;
+					Module	:    out Module_Ptr
+			);
+	
+		overriding
+		procedure Destroy(
+					Factory	: in out Static_Module_Factory;
+					Module	: in out Module_Ptr
+				);
+
+		The_Module	: constant Module_Ptr := new Static_Module;
+		Factory		: constant Module_Factory_Ptr := new Static_Module_Factory;
+	end Static_Modules;
+
+
+
+end KOW_View.Modules;
+
